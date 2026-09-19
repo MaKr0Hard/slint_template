ee7d101967c5c66efd69b719c377e0dd22a535a7
MaKr0Hard <258872817+MaKr0Hard@users.noreply.github.com>
2026-09-19 22:27:04 +0200

    Added some stuff, modified stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slint template (raplace with project name)
+
+Yes, Makefile + CMakeLists and the worst thing is that I'm proud of what I've done 
--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,15 @@
 
 int main(int argc, char **argv) {
 
-    // Instanciation de la fenêtre principale
     auto main_window = MainWindow::create();
 
-    // Connexion du callback défini dans le fichier .slint
+
     main_window->on_request_increase_value([&]() {
-        // Récupération de la valeur actuelle et incrémentation
         int current_val = main_window->get_counter();
         main_window->set_counter(current_val + 1);
     });
 
-    // Lancement de la boucle d'événements
     main_window->run();
-
+    // Don't need these comments imao
     return 0;
 }
